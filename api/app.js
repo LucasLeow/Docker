a8998a258ef25671aadd953c7cde3900223b7e06
LucasLeow <leowjianhoong@gmail.com>
2023-07-12 00:53:43 +0800

    Started small project & created Dockerfile to build image

---
+++ api/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.json([
+        {
+            "id": "1",
+            "title": "Book 1"
+        },
+        {
+            "id": "2",
+            "title": "Book 2"
+        }
+    ])
+});
+
+app.listen(4000, () => {
+    console.log("Listening to requests on port 4000");
+});
